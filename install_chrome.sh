462a404f59330fe9eca4a4e80b53e7fff3180b85
HunnyAroraH <parveenarora402@gmail.com>
2024-09-02 15:50:43 +0530

    a

--- install_chrome.sh
+++ install_chrome.sh
@@ -6,11 +6,8 @@ mkdir -p /tmp/chrome
 # Download the Chrome .deb package
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/chrome/google-chrome-stable_current_amd64.deb
 
-# Extract the .deb package manually
-ar x /tmp/chrome/google-chrome-stable_current_amd64.deb -C /tmp/chrome
-
-# Extract the data.tar.xz file to get the Chrome binary
-tar -xf /tmp/chrome/data.tar.xz -C /tmp/chrome
+# Extract the .deb package using dpkg-deb
+dpkg-deb -x /tmp/chrome/google-chrome-stable_current_amd64.deb /tmp/chrome/
 
 # Move the Chrome binary to /tmp/chrome/
 mv /tmp/chrome/opt/google/chrome/chrome /tmp/chrome/chrome
